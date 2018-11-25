33b4d1f17b630a476f67eb3a1aeadfdfde6631b0
98abhilash <98abhilash@gmail.com>
2018-11-25 20:03:05 +0530

    Update data_preprocessing.py

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -13,7 +13,7 @@ Y = dataset.iloc[:,3]
 #Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = "NaN", strategy = "mean", axis = 0)
-imputer = imputer.fit(x[:,1:3])
+imputer = imputer.fit(X[:,1:3])
 X[:,1:3] = imputer.transform(X[:,1:3])
 
 #Encoding Categorical data
